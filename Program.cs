13988311e3a5c84ea3db58e7b3145a14b1b277ad
mlac8 <mlac8@DESKTOP-IH3P6FS>
2021-06-18 01:13:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pick a card: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+        }
+    }
+}
